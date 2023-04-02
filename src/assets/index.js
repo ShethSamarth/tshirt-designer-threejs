fbb7975324b4a22637af20fb5866956e3ef2eef9
ShethSamarth <samarth.sheth.29@gmail.com>
2023-04-02 18:40:46 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import fileIcon from "./file.png"
+import swatch from "./swatch.png"
+import logoShirt from "./logo-tshirt.png"
+import stylishShirt from "./stylish-tshirt.png"
+
+export { fileIcon, swatch, logoShirt, stylishShirt }
